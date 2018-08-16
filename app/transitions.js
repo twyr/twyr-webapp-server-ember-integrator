0d3ca0457872b5a4f81dba0295be4fa6c6ffdfcb
Vish Desai <shadyvd@hotmail.com>
2018-08-16 15:02:40 +0530

    Minor changes

---
+++ app/transitions.js
@@ -0,0 +1,25 @@
+export default function() {
+	this.transition(
+		this.fromRoute(function() {
+			return true;
+		}),
+		this.toRoute(function() {
+			return true;
+		}),
+		this.use('crossFade', {
+			'duration': 300
+		}),
+	);
+
+	this.transition(
+		this.fromModel(function() {
+			return true;
+		}),
+		this.toModel(function() {
+			return true;
+		}),
+		this.use('crossFade', {
+			'duration': 300
+		}),
+	);
+}
