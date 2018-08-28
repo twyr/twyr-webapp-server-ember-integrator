561d6278b8d72134aea4d225a1f3f51524db6978
Vish Desai <shadyvd@hotmail.com>
2018-08-28 16:00:49 +0530

    Define Index / Application / Head HBS templates to make it easier for us to integrate in a modular way into the webapp-server

--- config/environment.js
+++ config/environment.js
@@ -72,33 +72,33 @@ module.exports = function (environment) {
 		}
 	};
 
-	if (environment === 'development') {
-		ENV.APP.LOG_RESOLVER = true;
-		ENV.APP.LOG_ACTIVE_GENERATION = true;
-		ENV.APP.LOG_TRANSITIONS = true;
-		ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-		ENV.APP.LOG_VIEW_LOOKUPS = true;
-	}
+	// if (environment === 'development') {
+	// 	ENV.APP.LOG_RESOLVER = true;
+	// 	ENV.APP.LOG_ACTIVE_GENERATION = true;
+	// 	ENV.APP.LOG_TRANSITIONS = true;
+	// 	ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+	// 	ENV.APP.LOG_VIEW_LOOKUPS = true;
+	// }
 
-	if (environment === 'test') {
-		// Testem prefers this...
-		ENV.locationType = 'none';
+	// if (environment === 'test') {
+	// 	// Testem prefers this...
+	// 	ENV.locationType = 'none';
 
-		// keep test console output quieter
-		ENV.APP.LOG_ACTIVE_GENERATION = false;
-		ENV.APP.LOG_VIEW_LOOKUPS = false;
+	// 	// keep test console output quieter
+	// 	ENV.APP.LOG_ACTIVE_GENERATION = false;
+	// 	ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-		ENV.APP.rootElement = '#ember-testing';
-		ENV.APP.autoboot = false;
-	}
+	// 	ENV.APP.rootElement = '#ember-testing';
+	// 	ENV.APP.autoboot = false;
+	// }
 
-	if (environment === 'production') {
-		ENV.APP.LOG_RESOLVER = false;
-		ENV.APP.LOG_ACTIVE_GENERATION = false;
-		ENV.APP.LOG_TRANSITIONS = false;
-		ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
-		ENV.APP.LOG_VIEW_LOOKUPS = false;
-	}
+	// if (environment === 'production') {
+	// 	ENV.APP.LOG_RESOLVER = false;
+	// 	ENV.APP.LOG_ACTIVE_GENERATION = false;
+	// 	ENV.APP.LOG_TRANSITIONS = false;
+	// 	ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
+	// 	ENV.APP.LOG_VIEW_LOOKUPS = false;
+	// }
 
 	return ENV;
 };
