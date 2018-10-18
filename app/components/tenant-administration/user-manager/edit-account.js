0ebf3bd335d0dc2509cc8098a84526bfa4646e16
Vish Desai <shadyvd@hotmail.com>
2018-10-18 19:20:23 +0530

    Next cut of Tenant Administration > User Manager. Basic User Account editing enabled

---
+++ app/components/tenant-administration/user-manager/edit-account.js
@@ -0,0 +1,4 @@
+import BaseComponent from '../../../framework/base-component';
+
+export default BaseComponent.extend({
+});
--- app/components/tenant-administration/user-manager/main-component.js
+++ app/components/tenant-administration/user-manager/main-component.js
@@ -1,7 +1,10 @@
+/* eslint-disable ember/no-on-calls-in-components */
+
 import BaseComponent from '../../../framework/base-component';
 import ExponentialBackoffPolicy from 'ember-concurrency-retryable/policies/exponential-backoff';
 
 import { observer } from '@ember/object';
+import { on } from '@ember/object/evented';
 import { task, timeout } from 'ember-concurrency';
 
 const backoffPolicy = new ExponentialBackoffPolicy({
@@ -73,29 +76,6 @@ export default BaseComponent.extend({
 		}
 	}).drop(),
 
-	changeAccountStatus: task(function* (tenantUser, newStatus) {
-		try {
-			const oldStatus = tenantUser.get('accessStatus');
-
-			tenantUser.set('operationIsRunning', true);
-			tenantUser.set('accessStatus', newStatus);
-
-			yield tenantUser.save();
-
-			const remainingUsersWithOldStatus = this.get('model').filterBy('accessStatus', oldStatus).get('length');
-			if(remainingUsersWithOldStatus) return;
-
-			yield this.get('onChangeAccordionItem').perform(undefined);
-		}
-		catch(err) {
-			tenantUser.rollback();
-			throw err;
-		}
-		finally {
-			tenantUser.set('operationIsRunning', false);
-		}
-	}).enqueue().retryable(backoffPolicy),
-
 	doResetPassword: task(function* (componentState) {
 		try {
 			componentState.tenantUser.set('operationIsRunning', true);
@@ -115,6 +95,105 @@ export default BaseComponent.extend({
 			componentState.tenantUser.set('operationIsRunning', false);
 		}
 	}).enqueue().retryable(backoffPolicy),
+
+	editAccount: task(function* (tenantUser) {
+		try {
+			const self = this;
+			const user = yield tenantUser.get('user');
+
+			const modalData = {
+				'title': 'Edit User Account',
+				'dialogClass': 'flex-100  flex-gt-md-75 flex-gt-lg-50',
+
+				'componentName': 'tenant-administration/user-manager/edit-account',
+				'componentState': {
+					'model': user
+				},
+
+				'confirmButton': {
+					'text': 'Save',
+					'icon': 'check',
+
+					'primary': true,
+					'raised': true,
+
+					'callback': () => {
+						self.get('doUpdateAccount').perform(user);
+					}
+				},
+
+				'cancelButton': {
+					'text': 'Cancel',
+					'icon': 'cancel',
+
+					'warn': true,
+					'raised': true,
+
+					'callback': () => {
+						user.rollback();
+					}
+				}
+			};
+
+			yield this.invokeAction('controller-action', 'displayModal', modalData);
+		}
+		catch(err) {
+			this.get('notification').display({
+				'type': 'error',
+				'error': err
+			});
+		}
+	}).drop(),
+
+	doUpdateAccount: task(function* (user) {
+		yield user.save();
+	}).enqueue().evented().retryable(backoffPolicy),
+
+	doUpdateAccountSucceeded: on('doUpdateAccount:succeeded', function(taskInstance) {
+		const user = taskInstance.args[0];
+		const loggedInUser = this.get('currentUser').getUser();
+
+		if(loggedInUser['user_id'] !== user.get('id'))
+			return;
+
+		window.TwyrApp.trigger('userChanged');
+	}),
+
+	doUpdateAccountErrored: on('doUpdateAccount:errored', function(taskInstance, err) {
+		const user = taskInstance.args[0];
+		user.rollback();
+
+		this.get('notification').display({
+			'type': 'error',
+			'error': err
+		});
+	}),
+
+	changeAccountStatus: task(function* (tenantUser, newStatus) {
+		const oldStatus = tenantUser.get('accessStatus');
+
+		tenantUser.set('operationIsRunning', true);
+		tenantUser.set('accessStatus', newStatus);
+
+		yield tenantUser.save();
+
+		const remainingUsersWithOldStatus = this.get('model').filterBy('accessStatus', oldStatus).get('length');
+		if(remainingUsersWithOldStatus) return;
+
+		yield this.get('onChangeAccordionItem').perform(undefined);
+	}).enqueue().evented().retryable(backoffPolicy),
+
+	changeAccountStatusErrored: on('changeAccountStatus:erored', function(taskInstance, err) {
+		const tenantUser = taskInstance.args[0];
+
+		tenantUser.rollback();
+		tenantUser.set('operationIsRunning', false);
+
+		this.get('notification').display({
+			'type': 'error',
+			'error': err
+		});
+	}),
 
 	onChangeAccordionItem: task(function* (newSelectedItem) {
 		this.set('selectedAccordionItem', newSelectedItem);
